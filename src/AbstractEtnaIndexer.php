8e0971cb9a96b9a891329bdd5b7904c9c39a71a2
Guillaume Dubost <dubost_g@etna-alternance.net>
2016-04-18 11:19:21 +0200

    [FEATURE#27818] Amelioration du provider
    Maintenant il est possible de gerer plusieurs types par indexes.
    
    Implementation de la fonction `createType` qui se comporte comme la fonction `createIndex` mais pour les types.
    Changement de l'`AbstractEtnaIndexer` qui implemente directement la fonction `reindex`

--- src/AbstractEtnaIndexer.php
+++ src/AbstractEtnaIndexer.php
@@ -7,13 +7,15 @@ use Silex\Application;
 abstract class AbstractEtnaIndexer
 {
     protected $app;
+    protected $name;
 
     /**
      * @param Application $app
      */
     public function __construct(Application $app, $name)
     {
-        $this->app = $app;
+        $this->app  = $app;
+        $this->name = $name;
 
         $this->app["elasticsearch.{$name}.reindex"]         = [$this, 'reindex'];
         $this->app["elasticsearch.{$name}.put_document"]    = [$this, 'putDocument'];
@@ -23,15 +25,32 @@ abstract class AbstractEtnaIndexer
     /**
      * @return void
      */
-    abstract public function reindex();
+    public function reindex($types = [])
+    {
+        if (!empty($invalid_types = array_diff($types, $this->app["elasticsearch.{$this->name}.types"]))) {
+            throw new \Exception("Invalid type(s) " . implode(', ', $types) . " for index {$this->name}");
+        }
+
+        if (empty($types)) {
+            $types = $this->app["elasticsearch.{$this->name}.types"];
+        }
+
+        foreach ($types as $type) {
+            $index_func_name = "index" . implode('', array_map('ucfirst', explode('_', $type)));
+            if (!method_exists($this, $index_func_name)) {
+                throw new \Exception("Implement the method {$index_func_name} as protected to index type {$type}");
+            }
+            $this->{$index_func_name}();
+        }
+    }
 
     /**
      * @return array
      */
-    abstract public function putDocument();
+    abstract public function putDocument($type);
 
     /**
      * @return void
      */
-    abstract public function removeDocument();
+    abstract public function removeDocument($type);
 }
